91185b2c8b595f849b7b42b83f83d72bed2b7528
kelmerhd <50677633+Kelmer-hd@users.noreply.github.com>
2024-02-18 11:17:53 -0500

    nuevos datos

--- primerosPasos/primerosPasos.js
+++ primerosPasos/primerosPasos.js
@@ -44,3 +44,37 @@ colores.push("Anaranjado");
 console.log(colores);
 
 
+
+/**************************************************************** */
+//                     Cadenas de Texto String
+/*******************************************************************/
+
+let nombre = "Jhon";
+let apellido = "Mircha";
+let saludo = new String("Hello");
+let lorem = "lorem"
+console.log(nombre.length,apellido.length,saludo.length);
+console.log(nombre.toUpperCase());
+console.log(apellido.toLocaleLowerCase());
+console.log(lorem.includes("amet")); // vefificar si incluye
+console.log(lorem.trim()); // eliminar espacios al inicio y final
+console.log(lorem.split(",")) // separa la cadena por el simbolo que le pasamos
+
+
+
+/**************************************************************** */
+//         TEMPLATES STRING, CONCATENACION, INTERPOLACION ETC
+/*******************************************************************/
+
+let nombre1 = "Jon";
+let apellido1 = "Mircha";
+let saludo1 = "Hola mi nombre es: " + nombre1 + " " + apellido1 + ".";
+console.log(saludo1);
+
+// Interpolacion 
+let saludo2 = `Hola mi nombre es ${nombre1} ${apellido1}.`;
+console.log(saludo2);
+
+
+
+
