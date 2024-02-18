c14335f676d7b952c930c1248f736c45e03c84e8
kelmerhd <50677633+Kelmer-hd@users.noreply.github.com>
2024-02-18 10:56:18 -0500

    primeros pasos en Js

---
+++ primerosPasos.js
@@ -0,0 +1,46 @@
+/*
+Valores Primitivos
+    string
+    number
+    boolean
+    null
+    undefined
+    Nan
+
+Valores Compuestos
+    objet = {}
+    array  []
+    function (){}
+    clase {}
+    etc
+*/
+
+
+// Variables en JavaScript
+var hola = "Hola mundo"; // 
+let hello = "Hola mundo con let"; // buena practica
+
+
+// Constantes en Javascript
+const PI = 3.1416;
+const NUEVE = 9;
+console.log(PI);
+
+// Objeto
+let objeto = {
+    nombre: "Jon",
+    edad: 25,
+}
+console.log(objeto)
+objeto.correo = "Jonmircha@gmail.com";
+
+
+// Arreglo
+let colores = [
+    "blanco", "negro", "azul"
+]
+
+colores.push("Anaranjado");
+console.log(colores);
+
+
